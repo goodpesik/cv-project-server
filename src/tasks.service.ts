93bbd694fff176ed04ac0d17dbeefac8f6a415a0
Maksym Levynets <mlevynets@drbsystems.com>
2025-04-13 08:57:04 +0300

    Added log

--- src/tasks.service.ts
+++ src/tasks.service.ts
@@ -1,21 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import * as fs from 'fs';
 import * as path from 'path';
 import { CvDataService } from './cv-data/cv-data.service';
 
 @Injectable()
-export class TasksService {
+export class TasksService implements OnModuleInit {
   constructor(private service: CvDataService) {}
   private readonly imagesPath = path.join(__dirname, '..', 'uploads');
 
+  onModuleInit() {
+    this.handleCleanup();
+  }
 
   @Cron('0 3 * * *')
   async handleCleanup() {
     console.log(`${new Date()}: Started Clean UP`)
     const images = (await this.service.findAll()).map((cv => cv.imageName));
 
-    if (!fs.existsSync(this.imagesPath)) return;
+    if (!fs.existsSync(this.imagesPath)) {
+      return console.error(`${new Date()}: Clean Task: Cannot find uploads folder`)
+    };
 
     const allFiles = fs.readdirSync(this.imagesPath);
 
